6338ab1dd892e4914dc4161f134bc8a6643a22d7
Leoodaviid <david_ax7@hotmail.com>
2024-07-21 22:03:22 -0300

    feat: authentication by clerk

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,29 @@
 import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/header";
 import { Button } from "@/components/ui/button";
 import React from "react";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
 const Home = () => {
   return (
     <div>
+      <Header>
+        <div className="flex w-fit items-center justify-center gap-2">
+          <p className="document-title">Share</p>
+        </div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </Header>
       <Editor />
     </div>
   );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-const Header = () => {
-  return <div>Header</div>;
+const Header = ({ children }: HeaderProps) => {
+  return (
+    <div className="header">
+      <Link href="/">
+        <Image
+          src="/assets/icons/logo.svg"
+          alt="Logo with name"
+          width={120}
+          height={32}
+          className="hidden md:block"
+        />
+        <Image
+          src="/assets/icons/logo.svg"
+          alt="Logo with name"
+          width={32}
+          height={32}
+          className="hidden md:hidden"
+        />
+      </Link>
+      {children}
+    </div>
+  );
 };
 
 export default Header;
